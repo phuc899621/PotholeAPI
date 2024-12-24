ff5b32de7714716f30ca228320b07c93e0d060e9
phuc899621 <phuc899621@gmail.com>
2024-12-24 17:42:10 +0700

    ds

--- app/routes/report.js
+++ app/routes/report.js
@@ -7,6 +7,8 @@ const SubInfo = require('../schemas/subinfo');
 const MainModel=require(__path_models+'users');
 const PotHoleModel=require(__path_models+'potholes');
 const ReportModel=require(__path_models+'report');
+const SubInfoModel=require(__path_models+'subinfo');
+
 //cau hinh luu tru hinh anh tam thoi
 const storage = multer.memoryStorage();  // Lưu trữ tạm thời trong bộ nhớ
 const upload = multer({ storage });
@@ -15,25 +17,34 @@ router.post('/add',upload.single('image'),async(req,res,next)=>{
       // Lấy dữ liệu hình ảnh từ file đã tải lên
       const {buffer } = req.file;
       const { email,description,location,severity } = JSON.parse(req.body.request);
+      if (!req.file || !req.body.request) {
+        return res.status(400).json({
+          success: false,
+          message: 'Missing image or request data',
+          data:[]
+        });
+      }
       // Tiến hành lưu ảnh vào MongoDB
       const _id=await MainModel.listUsers({'email':email},{'task':'email'});
-      let param0=[];
-      param0.location=location;
-      param0.reportedBy=_id;
-      param0.severity=severity;
-      const pothole=await PotHoleModel.addPothole({param});
+      let param0 = {
+        location,
+        reportedBy: _id,
+        severity,
+      };
+      const pothole=await PotHoleModel.addPothole(param0);
       const image = await PotHoleModel.editPothole({'_id':pothole._id,'image':buffer},{'task':'image'});
-      let param=[];
-      param.reportedBy=_id;
-      param.reportedInfo=pothole;
-      param.description=description;
-      const report=await ReportModel.addReport({param});
-      await SubInfo.update(data,{'task':'oneReport'});
+      let param={
+        reportedBy:_id,
+        reportedInfo:pothole,
+        description:description
+      };
+      const report=await ReportModel.addReport(param);
+      await SubInfoModel.update(pothole,{'task':'oneReport'});
       res.status(200).json({ 
         success:true,
         message: '',
-        data:[]});
-    } catch (error) {
+        data:[_id]});
+      }catch (error) {
       res.status(500).json({ 
         success:false,
         message: 'Error save report',
